d8e52e448f3fe64c842a1e1ee0fa227bacd4e146
Jnr <Jnr@192.168.1.100>
2014-07-25 22:52:37 -0500

    Primer comparacion entre repositorios funcionando -se agregaron librerias externas y la ultima verision del API propia -faltan detalles de comparacion

--- src/java/jnr/wsdlcomaparison/managerbeans/MB_TablaControl.java
+++ src/java/jnr/wsdlcomaparison/managerbeans/MB_TablaControl.java
@@ -10,6 +10,10 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import jnr.operationsmatcher.MatcherResult;
+import jnr.operationsmatcher.TreeMatcher;
 
 /**
  *
@@ -22,8 +26,38 @@ public class MB_TablaControl {
    
     private List<Registro> registros;
     private Registro registroSeleccionado;
+    private TreeMatcher comparador;
+
+    private String textoUno;
+    private String textoDos;
     
+    public String getTextoDos(){
+        return textoDos;
+    }
     
+    public void setTextoDos(String textoDos){
+        this.textoDos = textoDos;
+    }
+
+    /**
+     * Get the value of textoUno
+     *
+     * @return the value of textoUno
+     */
+    public String getTextoUno() {
+        return textoUno;
+    }
+
+    /**
+     * Set the value of textoUno
+     *
+     * @param textoUno new value of textoUno
+     */
+    public void setTextoUno(String textoUno) {
+        this.textoUno = textoUno;
+    }
+ 
+   
     public Registro getRegistroSeleccionado() {
         return registroSeleccionado;
     }
@@ -39,6 +73,7 @@ public class MB_TablaControl {
      * Creates a new instance of MB_TablaControl
      */
     public MB_TablaControl() {
+        comparador = new TreeMatcher();
         registros = new ArrayList<Registro>();
         registros.add(new Registro("SA00", "OA00", "SB00", "OB00", "S00"));
     }
@@ -49,7 +84,17 @@ public class MB_TablaControl {
             for(int j=1; j<10000;j++)
                 a= true;
         }
-        registros.add(new Registro("SA", "OA", "SB", "OB", "S"));
+                
+        List<MatcherResult> resultados;
+        resultados = comparador.matchWSDLDirectory_URL(textoUno);
+        for(int i=0; i<resultados.size(); i++){
+            registros.add(new Registro(resultados.get(i).getServicioA(), resultados.get(i).getServicioA(), resultados.get(i).getServicioB(), resultados.get(i).getEstructuraB(), Float.toString(resultados.get(i).getPorcentaje())));
+        }
+        
+        //comparador.matchWSDLAndDirectory("D:\\CINVESTAV\\Tesis\\servicios\\precisionRecall\\patron\\1personbicycle4wheeledcar_price_service.wsdl", "D:\\CINVESTAV\\Tesis\\servicios\\precisionRecall\\repositorio");
+              
+        
+        //jnr.utilities.Directorio.listarElementosDeURL("http://148.247.102.37:8080/wsdlcomparison/servicios/pruebas2/");
     } 
     
 }
