fd9e5d1aa3245c69a349f88e98a12ce764041155
Jnr <Jnr@192.168.1.100>
2014-07-28 19:58:58 -0500

    Funciones para comparativas WSDL listas -falta comparativa RDF(posible) -Agregar imagenes a detalle(posible)

--- src/java/jnr/wsdlcomaparison/managerbeans/MB_TablaControl.java
+++ src/java/jnr/wsdlcomaparison/managerbeans/MB_TablaControl.java
@@ -8,6 +8,7 @@ package jnr.wsdlcomaparison.managerbeans;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -29,35 +30,88 @@ public class MB_TablaControl {
     private Registro registroSeleccionado;
     private TreeMatcher comparador;
 
-    private String textoUno;
-    private String textoDos;
+    private String op1_wsdl1;
+    private String op1_wsdl2;
+    private boolean pruebaDosServicos;
     
-    public String getTextoDos(){
-        return textoDos;
-    }
+    private String op2_patron;
+    private String op2_repositorio;
+    private boolean pruebaServiciosServico;
     
-    public void setTextoDos(String textoDos){
-        this.textoDos = textoDos;
+    private String op3_repositorio;
+    private boolean pruebaRepositorio;
+
+   
+    
+    
+    public String getOp1_wsdl1() {
+        return op1_wsdl1;
     }
 
-    /**
-     * Get the value of textoUno
-     *
-     * @return the value of textoUno
-     */
-    public String getTextoUno() {
-        return textoUno;
+    public void setOp1_wsdl1(String op1_wsdl1) {
+        this.op1_wsdl1 = op1_wsdl1;
     }
 
-    /**
-     * Set the value of textoUno
-     *
-     * @param textoUno new value of textoUno
-     */
-    public void setTextoUno(String textoUno) {
-        this.textoUno = textoUno;
+    public String getOp1_wsdl2() {
+        return op1_wsdl2;
     }
- 
+
+    public void setOp1_wsdl2(String op1_wsdl2) {
+        this.op1_wsdl2 = op1_wsdl2;
+    }
+
+    public boolean isPruebaDosServicos() {
+        return pruebaDosServicos;
+    }
+
+    public void setPruebaDosServicos(boolean pruebaDosServicos) {
+        this.pruebaDosServicos = pruebaDosServicos;
+    }
+
+    public String getOp2_patron() {
+        return op2_patron;
+    }
+
+    public void setOp2_patron(String op2_patron) {
+        this.op2_patron = op2_patron;
+    }
+
+    public String getOp2_repositorio() {
+        return op2_repositorio;
+    }
+
+    public void setOp2_repositorio(String op2_repositorio) {
+        this.op2_repositorio = op2_repositorio;
+    }
+
+    public boolean isPruebaServiciosServico() {
+        return pruebaServiciosServico;
+    }
+
+    public void setPruebaServiciosServico(boolean pruebaServiciosServico) {
+        this.pruebaServiciosServico = pruebaServiciosServico;
+    }
+
+    public String getOp3_repositorio() {
+        return op3_repositorio;
+    }
+
+    public void setOp3_repositorio(String op3_repositorio) {
+        this.op3_repositorio = op3_repositorio;
+    }
+
+    public boolean isPruebaRepositorio() {
+        return pruebaRepositorio;
+    }
+
+    public void setPruebaRepositorio(boolean pruebaRepositorio) {
+        this.pruebaRepositorio = pruebaRepositorio;
+    }
+
+    
+    
+    
+    
    
     public Registro getRegistroSeleccionado() {
         return registroSeleccionado;
@@ -74,34 +128,100 @@ public class MB_TablaControl {
      * Creates a new instance of MB_TablaControl
      */
     public MB_TablaControl() {
+        op1_wsdl1 = op1_wsdl2 = op2_patron = op2_repositorio = op3_repositorio = ""; 
+        
         comparador = new TreeMatcher();
-        registros = new ArrayList<Registro>();
-        registros.add(new Registro("SA00", "OA00", "SB00", "OB00", "S00"));
+
+        //registros.add(new Registro("SA00", "OA00", "SB00", "OB00", "S00"));
     }
     
-    public void instertarRegistro(){
-        boolean a;
-        for(int i=1;i<100000; i++){
-            for(int j=1; j<10000;j++)
-                a= true;
-        }
+    
+    
+    public void compararWSDLUnoAUno(){
+        registros = new ArrayList<Registro>();
                 
         List<MatcherResult> resultados;
-        resultados = comparador.matchWSDLDirectory_URL(textoUno);
+        resultados = comparador.matchWSDLOnetoOne_URL(op1_wsdl1, op1_wsdl2);
         for(int i=0; i<resultados.size(); i++){
             registros.add(new Registro(resultados.get(i).getServicioA(), resultados.get(i).getServicioA(), resultados.get(i).getServicioB(), resultados.get(i).getEstructuraB(), Float.toString(resultados.get(i).getPorcentaje())));
         }
-        
-        //comparador.matchWSDLAndDirectory("D:\\CINVESTAV\\Tesis\\servicios\\precisionRecall\\patron\\1personbicycle4wheeledcar_price_service.wsdl", "D:\\CINVESTAV\\Tesis\\servicios\\precisionRecall\\repositorio");
-              
-        
-        //jnr.utilities.Directorio.listarElementosDeURL("http://148.247.102.37:8080/wsdlcomparison/servicios/pruebas2/");
+
+    }
+    
+    public void compararWSDLyRepositorio(){
+        registros = new ArrayList<Registro>();
+                
+        List<MatcherResult> resultados;
+        resultados = comparador.matchWSDLAndDirectory_URL(op2_patron, op2_repositorio);
+        for(int i=0; i<resultados.size(); i++){
+            registros.add(new Registro(resultados.get(i).getServicioA(), resultados.get(i).getServicioA(), resultados.get(i).getServicioB(), resultados.get(i).getEstructuraB(), Float.toString(resultados.get(i).getPorcentaje())));
+        }
+
+    }
+    
+    public void compararRepositorio(){
+        registros = new ArrayList<Registro>();
+                
+        List<MatcherResult> resultados;
+        resultados = comparador.matchWSDLDirectory_URL(op3_repositorio);
+        for(int i=0; i<resultados.size(); i++){
+            registros.add(new Registro(resultados.get(i).getServicioA(), resultados.get(i).getServicioA(), resultados.get(i).getServicioB(), resultados.get(i).getEstructuraB(), Float.toString(resultados.get(i).getPorcentaje())));
+        }
+
     } 
     
     public void insertarregristro2(){
         System.out.println("LEYENDO DIRECTORIO");
         Directorio.listarElementosDirectorio(".");
         registros.add(new Registro(" ", " ", " ", " ", " "));
+    }
+   
+    
+    public void setPruebaDosServicios(){
+        if(pruebaDosServicos){
+            op1_wsdl1 = "http://148.247.102.37:8080/wsdlcomparison/servicios/precisionRecall/repositorio/university_academic-support-staff_service.wsdl";
+            op1_wsdl2 = "http://148.247.102.37:8080/wsdlcomparison/servicios/precisionRecall/repositorio/_digitalstandardpriceprice_MediaMarktservice.wsdl";
+        }else{
+            op1_wsdl1 = op1_wsdl2 = "";
+        }
+        
+    }
+    
+    public void setPruebaServiciosServicio(){
+        if(pruebaServiciosServico){
+            op2_repositorio = "http://148.247.102.37:8080/wsdlcomparison/servicios/precisionRecall/repositorio/";
+            op2_patron = "http://148.247.102.37:8080/wsdlcomparison/servicios/precisionRecall/patron/1personbicycle4wheeledcar_price_service.wsdl";
+        }else{
+            op2_repositorio = op2_patron = "";
+        }
+    }
+   
+    public void setPruebaRepositorio(){
+        if(pruebaRepositorio){
+            op3_repositorio = "http://148.247.102.37:8080/wsdlcomparison/servicios/precisionRecall/repositorio/";
+        }else{
+            op3_repositorio = "";
+        }
+    }
+    
+    
+    
+    public static class Mensajes{
+            public static void infoStandard(String titulo, String mensaje) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, titulo, mensaje));
+            }
+
+            public static void warnStandard(String titulo, String mensaje) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, titulo, mensaje));
+            }
+
+            public static void errorStandard(String titulo, String mensaje) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, titulo, mensaje));
+            }
+
+            public static void fatalStandard(String titulo, String mensaje) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, titulo, mensaje));
+            }
     }
     
 }
